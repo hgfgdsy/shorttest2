3079a9e16ea0192547ebf6c854fbe0a94a662e40
hgfgdsy <1031010120@qq.com>
2021-09-24 21:20:02 +0800

    first

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/hgfgdsy/shorttest2
+
+go 1.15
+
+require (
+	github.com/hgfgdsy/shorttest1 main
+)
---
+++ t2.go
@@ -0,0 +1,17 @@
+package shorttest2
+
+import(
+	"fmt"
+
+	"github.com/hgfgdsy/shorttest1"
+)
+
+
+func t2(){
+	fmt.Println("789")
+	shorttest1.test1()
+}
+
+func test2(){
+	fmt.Println("456")
+}
